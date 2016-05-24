16b2e45c55b7fa7af363ea328c2116237c73b115
thaddeusleonardo <thaddeus_leonardo@dlsu.edu.ph>
2016-05-24 16:39:26 +0800

    Create HelloFromThaddeus.c

---
+++ HelloFromThaddeus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ puts("()()()()()()()()()()()()()()()()()()()");
+  puts("   Thaddeus Angelico S. Leonardo");
+  puts("             COE-CIV");
+  puts("             11345276");
+  puts("          July 13, 1996");
+  puts("   thaddeus_leonardo@dlsu.edu.ph");
+  puts("()()()()()()()()()()()()()()()()()()()");
+return 0;
+}
